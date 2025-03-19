b5bfa51cc576abfeb24ae19c524693f839662a7d
xenia1w <xenia.wysokinska@gmail.com>
2025-03-19 17:44:41 +0100

    added download button

--- invoice_details.py
+++ invoice_details.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import requests
 import pandas as pd
 from api_calls import(
-    fetch_invoice_details
+    fetch_invoice_details, authorize
 )
+
+def download_document(file_url):
+    pass
 
 @st.dialog("Detailed view of the selected item")
 def display_details(file_id):
@@ -34,3 +38,34 @@ def display_details(file_id):
     st.write(f"**Total Brutto:** {invoice_details.get('brutto')}")
     st.write(f"**Open Amount (Balance):** {invoice_details.get('balance')}")
     st.write(f"**Total VAT:** {invoice_details.get('total_vat')}")
+
+    file_url = invoice_details.get("file", {}).get("file_url")
+    filename = invoice_details.get("file", {}).get("download_filename", "invoice.pdf")
+
+    if file_url:
+        auth_response = authorize()
+
+        if auth_response.status_code == 200:
+            access_token = auth_response.json().get("access_token")
+
+            headers = {
+                "Authorization": f"Bearer {access_token}",
+                "Content-Type": "application/json"
+            }
+
+            download_response = requests.get(file_url, headers=headers)
+
+            if download_response.status_code == 200:
+                    # Automatically trigger download when the invoice is selected
+                    st.download_button(
+                        label="📥 Click to Download Invoice",
+                        data=download_response.content,
+                        file_name=filename,
+                        mime="application/pdf"
+                    )
+            else:
+                st.error(" Failed to fetch the invoice file.")
+        else:
+            st.warning("Could not authenticate request.")
+    else:
+        st.warning("No invoice file available.")
